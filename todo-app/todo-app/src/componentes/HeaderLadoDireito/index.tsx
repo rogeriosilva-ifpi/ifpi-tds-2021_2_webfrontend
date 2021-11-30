dd6f6010cf0544c9f9b85e8efe4462cbbf698fc0
Rogério Silva <rogerio.silva@ifpi.edu.br>
2021-11-30 10:20:53 -0300

    HEader e Botao Ok

---
+++ todo-app/todo-app/src/componentes/HeaderLadoDireito/index.tsx
@@ -0,0 +1,19 @@
+
+import estilos from './estilos.module.css'
+
+
+export function HeaderLadoDireito(){
+  return (
+    <div className={estilos.header_lado_direito}>
+      <div className={estilos.bloco}>
+        <p>10</p>
+        <span>Abertas</span>
+      </div>
+      <div className={estilos.bloco}>
+        <p>12</p>
+        <span>Feitas</span>
+      </div>
+    </div>
+    
+  )
+}
---
+++ todo-app/todo-app/src/componentes/HeaderLadoEsquerdo/index.tsx
@@ -0,0 +1,10 @@
+import estilos from './estilos.module.css'
+
+export function HeaderLadoEsquerdo(){
+  return (
+    <div className={estilos.header_lado_esquerdo}>
+      <p>AppTarefas</p>
+    </div>
+    
+  )
+}
